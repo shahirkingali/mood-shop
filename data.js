ac587888951ff7b92d0428c5a94400976161d22d
Shahir Ali <shahirsheikali@gmail.com>
2020-10-06 23:42:11 -0700

    update

---
+++ data.js
@@ -0,0 +1,79 @@
+const itemsContainer = document.getElementById('items')
+import data from './data.js'
+
+
+
+const data = [{
+    "id": 1,
+    "name": "happy",
+    "image": "./resources/images/happy_spongebob.gif",
+    "desc": "Happiness lies in the joy of achievement and the thrill of creative effort.",
+    "price": 5.99
+  }, {
+    "id": 2,
+    "name": 'sad',
+    "image": './resources/images/sad_spongebob.gif',
+    "desc": "Tears are words that can’t be explained.",
+    "price": 5.99
+  }
+  ...
+
+  // the length of our data determines how many times this loop goes around
+for (let i=0; i<data.length; ++i) {
+    // create a new div element and give it a class name
+    let newDiv = document.createElement('div');
+    newDiv.className = 'item'
+
+    // create an image element
+    let img = document.createElement('img');
+    // this will change each time we go through the loop. Can you explain why?
+    img.src = data[i].image
+    img.width = 300
+    img.height = 300
+
+    // Add the image to the div
+    newDiv.appendChild(img)
+    console.log(img)
+}
+
+itemsContainer.appendChild(newDiv)
+
+// create a paragraph element for a description
+let desc = document.createElement('P')
+// give the paragraph text from the data
+desc.innerText =data[i].desc
+// append the paragraph to the div
+newDiv.appendChild(desc)
+// do the same thing for price
+let price = document.createElement('P')
+price.innerText = data[i].price
+newDiv.appendChild(price)
+
+for (let i=0; i<data.length; ++i) {
+    let newDiv = document.createElement('div');
+      newDiv.className = 'item'
+    // display the image
+    let img = document.createElement('img');
+    img.src = data[i].image
+    img.width = 300
+    img.height = 300
+    newDiv.appendChild(img)
+  
+    let desc = document.createElement('P')
+    desc.innerText =data[i].desc
+    newDiv.appendChild(desc)
+    let price = document.createElement('P')
+    price.innerText = data[i].price
+    newDiv.appendChild(price)
+  
+    let button = document.createElement('button')
+    button.id = data[i].name
+  
+    // creates a custom attribute called data-price.
+    // That will hold the price for each element in the button
+    button.dataset.price = data[i].price
+    button.innerHTML = "Add to Cart"
+    newDiv.appendChild(button)
+    // put new div inside items container
+    itemsContainer.appendChild(newDiv)
+  }
